de22898b555a6e739ffaab0c21edd5d404af01a9
gva <gvanastasov@abv.bg>
2017-02-03 00:26:29 +0100

    task 1 finished, ex setup

---
+++ Ex2-HTTP/sln/ex2_http/ByTheCake/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByTheCake
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Content-Type: text/html \n\r");
+            Console.WriteLine(@"<!DOCTYPE html>
+                                <html lang=""en"">
+                                    <head>
+                                        <title>By The Cake</title>
+                                        <meta charset=""UTF-8"">
+                                    </head>
+                                    <body>
+                                        <h3>By The Cake</h3>
+                                        <h4 style=""border-bottom: 1px solid black"">Enjoy our awesome cakes</h4>
+                                    </body>
+                                </html>");
+        }
+    }
+}
