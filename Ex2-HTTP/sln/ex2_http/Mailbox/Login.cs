64a7f4afbfd91dce0a66b0f35f94da3b9f2e82bf
gva <gvanastasov@abv.bg>
2017-02-05 19:18:02 +0100

    finished sendemail

---
+++ Ex2-HTTP/sln/ex2_http/Mailbox/Login.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mailbox
+{
+    class Login
+    {
+        static void Main()
+        {
+            Console.WriteLine("Content-type: text/html\n\r");
+
+            Console.WriteLine("<!DOCTYPE html> <html lang=\"en\"> <head> <title>Send Email</title> <meta charset=\"UTF-8\"> </head> <body>");
+
+            var request = Environment.GetEnvironmentVariable("REQUEST_METHOD");
+
+            Console.WriteLine("Request type: " + request);
+
+            if (request == "GET")
+            {
+                GetLoginForm();
+            }
+            else if(request == "POST")
+            {
+                var credentials = Console.ReadLine();
+                var uValid = "123";
+                var pValid = "123";
+
+                if(credentials != "")
+                {
+                    string[] tokens = credentials.Split(new char[] { '&', '=' }, StringSplitOptions.RemoveEmptyEntries);
+                    string username = tokens[1];
+                    string password = tokens[3];
+
+                    if(username == uValid && password == pValid)
+                    {
+                        PostLoginForm();
+                    }
+                    else
+                    {
+                        GetLoginForm();
+                        Console.WriteLine("<div>Invalid username or password!</div>");
+                    }
+                }
+
+            }
+
+            Console.WriteLine("</body></html>");
+        }
+
+        private static void GetLoginForm()
+        {
+            Console.WriteLine("<h2>Login</h2>");
+            Console.WriteLine(@"<form action=""Mailbox.exe"" method=""post"">
+                                    <label for="""">Username</label>
+                                    <input type=""text"" name=""username""><br>
+                                    <label for="""">Password</label>
+                                    <input type=""password"" name=""pass""><br>
+                                    <input type=""submit"" value=""Login""><br>
+                                </form>");
+        }
+
+        private static void PostLoginForm()
+        {
+            Console.WriteLine(File.ReadAllText("temp.html"));
+        }
+
+        private static void PostEmailForm()
+        {
+
+        }
+    }
+}
---
+++ Ex2-HTTP/sln/ex2_http/Mailsend/Mailsend.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Mailsend
+{
+    class Mailsend
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Content-type: text/html\n\r");
+
+            Console.WriteLine("<form action=\"Login.exe\" method=\"post\">");
+
+            Console.WriteLine("Username<input type=\"text\" name=\"username\"><br>");
+            Console.WriteLine("Password<input type=\"password\" name=\"pass\"><br>");
+            Console.WriteLine("<input type=\"submit\" value\"Login\">");
+
+            Console.WriteLine("</form>");
+        }
+    }
+}
