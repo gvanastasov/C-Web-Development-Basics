f6fb80ca8015568c04a585f528a5b32bf1f28d57
gva <gvanastasov@abv.bg>
2017-02-08 22:34:23 +0100

    playing around with settings

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
     webserver = require('gulp-webserver');
 
 gulp.task('webserver', function () {
-    gulp.src('./Repository/Ex3-Bootstrap/')
+    gulp.src('./Ex3-Bootstrap/')
         .pipe(webserver({
             livereload: true,
             open: true,
