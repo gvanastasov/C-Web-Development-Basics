6493d37863947bc6111bd672dbcf76d2125e4efc
gva <gvanastasov@abv.bg>
2017-02-12 18:53:39 +0100

    added db context

---
+++ Ex4-State_Management/sln/PizzaMore/PizzaMore.Data/PizzaMoreContext.cs
@@ -0,0 +1,19 @@
+﻿namespace PizzaMore.Data
+{
+    using Models;
+    using System.Data.Entity;
+
+    public class PizzaMoreContext : DbContext
+    {
+        public PizzaMoreContext()
+            : base("name=PizzaMoreContext")
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        public virtual DbSet<Session> Sessions { get; set; }
+
+        public virtual DbSet<Pizza> PizzaSuggestions { get; set; }
+    }
+}
