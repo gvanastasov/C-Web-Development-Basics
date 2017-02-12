e52e9d4622b7b0ec6d03881e15788383453481fa
gva <gvanastasov@abv.bg>
2017-02-12 18:18:07 +0100

    added utils lib

---
+++ Ex4-State_Management/sln/PizzaMore/PizzaMore.Utility/CookieCollection.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaMore.Utility
+{
+    class CookieCollection : ICookieCollection
+    {
+        public IDictionary<string, Cookie> Cookies { get; private set; }
+
+        public CookieCollection()
+        {
+            this.Cookies = new Dictionary<string, Cookie>();
+        }
+
+        public Cookie this[string key]
+        {
+            get
+            {
+                return Cookies[key];
+            }
+
+            set
+            {
+                if (this.Cookies.ContainsKey(key))
+                {
+                    this.Cookies[key] = value;
+                }
+                else
+                {
+                    this.Cookies.Add(key, value);
+                }
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return Cookies.Count;
+            }
+        }
+
+        public void AddCookie(Cookie cookie)
+        {
+            if (!this.Cookies.ContainsKey(cookie.Name))
+            {
+                this.Cookies.Add(cookie.Name, new Cookie());
+            }
+
+            this.Cookies[cookie.Name] = cookie;
+        }
+
+        public bool ContainsKey(string key)
+        {
+            return Cookies.ContainsKey(key);
+        }
+
+        public void RemoveCookie(string cookieName)
+        {
+            if (this.Cookies.ContainsKey(cookieName))
+            {
+                this.Cookies.Remove(cookieName);
+            }
+        }
+    }
+}
---
+++ Ex4-State_Management/sln/PizzaMore/PizzaMore.Utility/ICookieCollection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaMore.Utility
+{
+    interface ICookieCollection
+    {
+        void AddCookie(Cookie cookie);
+
+        void RemoveCookie(string cookieName);
+
+        bool ContainsKey(string key);
+
+        int Count { get; }
+
+        Cookie this[string key] { get; set; }
+    }
+}
