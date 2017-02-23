148d4f9d24b4bb4c6577cdfa5f96b5a76b4ad1c7
gva <gvanastasov@abv.bg>
2017-02-23 23:21:55 +0100

    http req/resp objects

---
+++ Ex5-Handmade Http Server/Handmade Http Server/SimpleHttpServer/Models/HttpRequest.cs
@@ -0,0 +1,37 @@
+﻿using SimpleHttpServer.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHttpServer.Models
+{
+    public class HttpRequest
+    {
+        public RequestMethod Method { get; set; }
+        public string Url { get; set; }
+        public Header Header { get; set; }
+        public string Content { get; set; }
+
+        public HttpRequest()
+        {
+            this.Header = new Header(HeaderType.HttpRequest);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder req = new StringBuilder();
+
+            req.AppendLine($"{Method} {Url} HTTP/1.0");
+            req.AppendLine($"{Header}");
+
+            if(string.IsNullOrEmpty(Content) == false)
+            {
+                req.AppendLine($"{Content}");
+            }
+
+            return req.ToString();
+        }
+    }
+}
---
+++ Ex5-Handmade Http Server/Handmade Http Server/SimpleHttpServer/Models/HttpResponse.cs
@@ -0,0 +1,35 @@
+﻿using SimpleHttpServer.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHttpServer.Models
+{
+    public class HttpResponse
+    {
+        public ResponseStatusCode StatusCode { get; set; }
+        public string StatusMessage
+        {
+            get { return StatusCode.ToString(); }
+        }
+        public Header Header { get; set; }
+        public byte[] Content { get; set; }
+
+        public HttpResponse()
+        {
+            this.Header = new Header(HeaderType.HttpResponse);
+        }
+
+        public override string ToString()
+        {
+            var response = new StringBuilder();
+
+            response.AppendLine($"HTTP/1.0 {StatusCode} {StatusMessage}");
+            response.AppendLine($"{Header}");
+
+            return response.ToString();
+        }
+    }
+}
