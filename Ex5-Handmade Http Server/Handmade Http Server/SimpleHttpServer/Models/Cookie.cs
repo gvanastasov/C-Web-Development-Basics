d79785728c9bdef35c5b3b93a48c4121cc84994a
gva <gvanastasov@abv.bg>
2017-02-23 22:17:38 +0100

    added cookies classes

---
+++ Ex5-Handmade Http Server/Handmade Http Server/SimpleHttpServer/Models/Cookie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHttpServer.Models
+{
+    /// <summary>
+    /// kvp type that hold user specific page data/settings
+    /// </summary>
+    public class Cookie
+    {
+        public string Name { get; private set; }
+        public string Value { get; private set; }
+
+        public Cookie() : this(null, null)
+        {
+
+        }
+        public Cookie(string name, string value)
+        {
+            this.Name = name;
+            this.Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name}={this.Value}";
+        }
+    }
+}
---
+++ Ex5-Handmade Http Server/Handmade Http Server/SimpleHttpServer/Models/CookieCollection.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHttpServer.Models
+{
+    public class CookieCollection : IEnumerable<Cookie>
+    {
+        // name -> cookie
+        public IDictionary<string, Cookie> Cookies { get; private set; }
+        public int Count
+        {
+            get { return this.Cookies.Count; }
+        }
+        public Cookie this[string key]
+        {
+            get
+            {
+                return this.Cookies[key];
+            }
+            set
+            {
+                if (this.Contains(key))
+                {
+                    this.Cookies[key] = value;
+                }
+                else
+                {
+                    this.Cookies.Add(key, value);
+                }
+            }
+        }
+
+
+
+
+        public CookieCollection()
+        {
+            this.Cookies = new Dictionary<string, Cookie>();
+        }
+
+        public void AddCookie(Cookie cookie)
+        {
+            if(this.Cookies.ContainsKey(cookie.Name) == false)
+            {
+                this.Cookies.Add(cookie.Name, new Cookie());
+            }
+
+            this.Cookies[cookie.Name] = cookie;
+        }
+
+        public bool Contains(string key)
+        {
+            return Cookies.ContainsKey(key);
+        }
+
+        
+
+        public IEnumerator<Cookie> GetEnumerator()
+        {
+            return this.Cookies.Values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            return string.Join("; ", Cookies.Values);
+        }
+    }
+}
