c23df3cf76e8a7b74dc283e0cfca911cb504a154
gva <gvanastasov@abv.bg>
2017-02-23 23:06:01 +0100

    request method type and status codes

---
+++ Ex5-Handmade Http Server/Handmade Http Server/SimpleHttpServer/Enums/ResponseStatusCode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHttpServer.Enums
+{
+    public enum ResponseStatusCode
+    {
+        // 1xx - Information
+        // 2xx - Success
+        // 3xx - Redirection
+        // 4xx - Cleint Error
+        // 5xx - Server Error
+
+        Continue = 100,
+        OK = 200,
+        Created = 201,
+        Accepted = 202,
+        MovedPermanently = 301,
+        Found = 302,
+        NotModified = 304,
+        BadRequest = 400,
+        Unauthorized = 401,
+        Forbidden = 403,
+        NotFound = 404,
+        MetodNotAllowed = 405,
+        InternalServerError = 500
+    }
+}
