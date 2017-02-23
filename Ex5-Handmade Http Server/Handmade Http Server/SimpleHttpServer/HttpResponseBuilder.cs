1e7858011d0a53daf4f3d248dd7e44d9add957e5
gva <gvanastasov@abv.bg>
2017-02-23 23:40:19 +0100

    added 404/500 views and response builder

---
+++ Ex5-Handmade Http Server/Handmade Http Server/SimpleHttpServer/HttpResponseBuilder.cs
@@ -0,0 +1,34 @@
+﻿using SimpleHttpServer.Enums;
+using SimpleHttpServer.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleHttpServer
+{
+    public static class HttpResponseBuilder
+    {
+        public static HttpResponse InternalServerError()
+        {
+            //string content = File.ReadAllText("Resources/Pages/500.html");
+            var response = new HttpResponse()
+            {
+                StatusCode = ResponseStatusCode.InternalServerError,
+            };
+            response.Content = File.ReadAllBytes("./Pages/500.html");
+
+            return response;
+        }
+
+        public static HttpResponse NotFound()
+        {
+            var response = new HttpResponse();
+            response.Content = File.ReadAllBytes("./Pages/404.html");
+
+            return response;
+        }
+    }
+}
