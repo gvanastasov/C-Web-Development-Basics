42aa0750fcfa09a6f52108aa3119f4c177c99bf5
svetlalt <svetla_lt@abv.bg>
2017-03-03 23:23:54 +0100

    added models and db context

---
+++ Ex6-SimpleMVC/SimpleMVC/SimpleMVC.App/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMVC.App.Models
+{
+    public class User
+    {
+        public User()
+        {
+            this.Notes = new HashSet<Note>();
+        }
+
+        public int Id { get; set; }
+        public string Username { get; private set; }
+        public string Passsword { get; private set; }
+        IEnumerable<Note> Notes { get; set; }
+    }
+}
